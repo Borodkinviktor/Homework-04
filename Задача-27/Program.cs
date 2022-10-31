11d83da2314e2aa98ba1a164bb905c89bf5b0ca0
Borodkin <borodkinviktor@gmail.com>
2022-10-31 11:47:43 +0300

    Домашняя работа №4

---
+++ Задача-27/Program.cs
@@ -0,0 +1,15 @@
+﻿ /* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int summ = 0;
+
+while(number > 0)
+{
+    summ = summ + number % 10;
+    number = (number - number % 10) / 10;
+}
+Console.WriteLine($"Сумма цифр равна {summ}");
